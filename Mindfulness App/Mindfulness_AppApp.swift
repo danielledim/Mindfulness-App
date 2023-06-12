81f7af02df3d6889547e69148e47b07561fc5823
Danielle DiMonte <dimonteda@gmail.com>
2023-06-12 10:42:42 -0500

    Initial Commit

---
+++ Mindfulness App/Mindfulness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindfulness_AppApp.swift
+//  Mindfulness App
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Mindfulness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
